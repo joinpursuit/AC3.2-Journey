7b2c8e08e56dcccb01a39d9fdee5f2573748bc7a
Jermaine Kelly <jerjunkel@gmail.com>
2016-11-08 12:59:55 -0500

    Added Base Project

---
+++ Journey/Journey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Journey
+//
+//  Created by Jermaine Kelly on 11/8/16.
+//  Copyright © 2016 Jermaine Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
