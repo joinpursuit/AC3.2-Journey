ed97e3771e309124d57a93fa647109c2be90a525
Jermaine Kelly <jerjunkel@gmail.com>
2016-11-09 22:01:48 -0500

    Added ew view controller files

--- Journey/Journey/BusinessResultsTableViewController.swift
+++ Journey/Journey/BusinessResultsTableViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class BusinessResultsTableViewController: UITableViewController {
 
+    var businessResults: [Business]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,6 +20,7 @@ class BusinessResultsTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
